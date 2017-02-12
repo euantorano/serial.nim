0fc10661403a526f8cd7bf3c5ab389f40803107c
Euan Torano <euantorano@gmail.com>
2017-02-12 10:14:50 +0000

    Update docs to include `listSerialPorts`.
    Signed-off-by: Euan Torano <euantorano@gmail.com>

--- src/serialport/private/list_serialports.nim
+++ src/serialport/private/list_serialports.nim
@@ -1,9 +1,11 @@
 {.deadCodeElim:on.}
 
-when defined(windows):
+when defined(nimdoc):
+  iterator listSerialPorts*(): string {.raises:[OSError].} = discard
+    ## Iterate through a list of the available serial port names on the system.
+elif defined(windows):
   include ./list_serialports_win
 elif defined(macosx):
-  # TODO: Mac support
   include ./list_serialports_mac
 else:
   {.error: "Unsupported OS".}
