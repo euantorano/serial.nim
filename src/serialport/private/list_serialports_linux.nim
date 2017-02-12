9a7eb33f53d30bfeae625642b5e399bff8f0ad0c
Euan Torano <euantorano@gmail.com>
2017-02-12 15:19:07 +0000

    Working on listing ports in Linux
    Signed-off-by: Euan Torano <euantorano@gmail.com>

--- src/serialport/private/list_serialports_linux.nim
+++ src/serialport/private/list_serialports_linux.nim
@@ -2,12 +2,29 @@
 
 import os
 
-const deviceGrepPaths = ["dev/ttyS*", "/dev/ttyUSB*", "/dev/ttyACM*", "/dev/ttyAMA*", "/dev/rfcomm*"]
+const deviceGrepPaths = ["/dev/ttyS*", "/dev/ttyUSB*", "/dev/ttyACM*", "/dev/ttyAMA*", "/dev/rfcomm*"]
 
-proc checkPath(path: string): bool = false
+proc checkPath(path: string): bool =
+  let fileName = extractFilename(path)
+  let fullDevicePath = "/sys/class/tty" / filename / "device"
+
+  var devicePath: string = "",
+    subsystem: string = ""
+
+  if existsFile(fullDevicePath):
+    devicePath = expandFilename(fullDevicePath)
+    subsystem = extractFilename(expandFilename("/sys/class/tty" / filename / "subsystem"))
+  else:
+    devicePath = ""
+
+  if subsystem != "platform":
+    result = true
+
+  echo "Got subsystem '", subsystem, "' device path: ", devicePath, " for file: ", path
+
 
 iterator getPortsForPath(path: string): string {.raises: [OSError].} =
-  for f in walkFiles(path):
+  for f in walkPattern(path):
     echo "Path: ", f
     if checkPath(f):
       yield f
