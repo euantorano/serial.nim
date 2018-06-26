c0faaa8f254be2c622723dff83fbe3e7397b4b4b
Euan T <euantorano@gmail.com>
2018-06-26 14:12:30 +0100

    Add wrapper procs for reading and writing

--- src/serial/serialport.nim
+++ src/serial/serialport.nim
@@ -6,3 +6,17 @@ elif defined(posix):
   include ./private/serialport/serialport_posix
 else:
   {.error: "Serial port handling not implemented for your platform".}
+
+proc read*(port: SerialPort, buff: var string): int32 =
+  ## Read from the serial port into the buffer `buff`. This will return the actual number of bytes that were received.
+  if isNil(buff) or len(buff) == 0:
+    return 0
+
+  result = port.read(addr buff[0], int32(len(buff)))
+
+proc write*(port: SerialPort, buff: var string): int32 =
+  ## Write the data to the serialport `port` from the buffer `buff`. This will return the number of bytes that were written.
+  if isNil(buff) or len(buff) == 0:
+    return 0
+
+  result = port.write(addr buff[0], int32(len(buff)))
