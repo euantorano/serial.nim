a36a58156221e7c11c77aac662181e58d26bbb97
Mark Pointing <sheriff.pointing@gmail.com>
2019-12-13 02:03:09 +1100

    Fix issue  #26 (some usb ports not enumerated on Win7) via a registry scan (#33)
    * Got windows version compiling.
    
    * Changed fixes to use cast instead of changing types.
    
    * Fixed incorrect type left over from applying cast fix.
    
    * Fixed timeout settings on windows version.
    
    * Fixed edge case where setting write timeout to 0 in windows results in infinite timeout.
    
    * Added a comment explaining write timeout behavior in windows.
    
    * Modified posix implementation so that zero read timeout behaves the same as windows implementation.
    
    * Fixed posix read (with timeout) so that it attempts to get the requested number of chracters before returning.  Fixed bug in posix read timeout where only timeval.tv_usec was used even when timeout was greater than 1 second.
    
    * Cleaned up runaway whitespace.
    
    * Cleaned up runaway link pasting.
    
    * Implemented registry scanning for USB serial ports on windows.
    
    * Removed some USB device filters accidentally copied over from the project where I originally implemented the registry scanning.
    
    * Changed some var to let, used Hashset for portnames.
    
    * Canged implementation to use SERIALCOMM node.

--- src/serial/private/utils/list_serialports_windows.nim
+++ src/serial/private/utils/list_serialports_windows.nim
@@ -1,80 +1,15 @@
 # Windows specific code to list available serial ports using SetupDiGetClassDevs().
 
-import winlean, os, registry
-
-type
-  HDEVINFO = ptr HANDLE
-  HKEY = uint
-  REGSAM = DWORD
-  SP_DEVINFO_DATA {.final, pure.} = object
-    cbSize: DWORD
-    ClassGuid: GUID
-    DevInst: DWORD
-    Reserved: pointer
-
-const
-  DIGCF_PRESENT: uint = 0x00000002
-  DIGCF_DEVICEINTERFACE: uint = 0x00000010
-  setupDiGetClassDevsInitialFlags: DWORD = DWORD(DIGCF_PRESENT or DIGCF_DEVICEINTERFACE)
-  DICS_FLAG_GLOBAL: DWORD = 1
-  DIREG_DEV: DWORD = 1
-  KEY_QUERY_VALUE: REGSAM = 1
-  NULL_HANDLE: uint = 0
-
-var
-  GUID_DEVINTERFACE_COMPORT: GUID = GUID(D1: 0x86e0d1e0'i32, D2: 0x8089'i16,
-    D3: 0x11d0, D4: [
-    0x9c'i8, 0xe4'i8, 0x08'i8, 0x00'i8, 0x3e'i8, 0x30'i8, 0x1f'i8, 0x73'i8])
-
-proc SetupDiGetClassDevs(ClassGuid: ptr GUID, Enumerator: cstring,
-    hwndParent: uint, Flags: DWORD): HDEVINFO
-  {.stdcall, importc: "SetupDiGetClassDevsW", dynlib: "setupapi.dll".}
-
-proc SetupDiEnumDeviceInfo(DeviceInfoSet: HDEVINFO, MemberIndex: DWORD,
-    DeviceInfoData: ptr SP_DEVINFO_DATA): bool
-  {.stdcall, importc: "SetupDiEnumDeviceInfo", dynlib: "setupapi.dll".}
-
-proc SetupDiOpenDevRegKey(DeviceInfoSet: HDEVINFO,
-    DeviceInfoData: ptr SP_DEVINFO_DATA, Scope: DWORD, HwProfile: DWORD,
-    KeyType: DWORD, samDesired: REGSAM): HKEY
-  {.stdcall, importc: "SetupDiOpenDevRegKey", dynlib: "setupapi.dll".}
-
-proc SetupDiDestroyDeviceInfoList(DeviceInfoSet: HDEVINFO): bool
-  {.stdcall, importc: "SetupDiDestroyDeviceInfoList", dynlib: "setupapi.dll".}
+import windows_registry
 
 iterator listSerialPorts*(): string =
   ## Iterates through a list of serial port names currently available on the system.
-  ##
-  ## This is based upon the `CEnumerateSerial::QueryUsingSetupAPI` method from `CEnumerateSerial`: http://www.naughter.com/enumser.html
 
-  # First create a "device info set" for the device interface GUID
-  let devInfoSet = SetupDiGetClassDevs(addr GUID_DEVINTERFACE_COMPORT, nil,
-      NULL_HANDLE, setupDiGetClassDevsInitialFlags)
-  if devInfoSet[] == -1:
-    raiseOsError(osLastError())
-
+  # the SERIALCOMM node does not exist if no serial 
+  # devices have been plugged in since bootup hence the 'try'. 
   try:
-    var
-      moreItems: bool = true
-      index: DWORD = 0
-      devInfo: SP_DEVINFO_DATA
+    for k, v in enumKeyValues(r"HARDWARE\DEVICEMAP\SERIALCOMM", HKEY_LOCAL_MACHINE):
+      yield v 
+  except OSError:
+    discard 
 
-    ## Then enumerate the entries in the device info set
-    while moreItems:
-      devInfo.cbSize = DWORD(sizeof(SP_DEVINFO_DATA))
-      moreItems = SetupDiEnumDeviceInfo(devInfoSet, index, addr devInfo)
-
-      if moreItems:
-        # Open the registry key for the device
-        let regKey: HKEY = SetupDiOpenDevRegKey(devInfoSet, addr devInfo,
-            DICS_FLAG_GLOBAL, 0, DIREG_DEV, KEY_QUERY_VALUE)
-
-        if regKey != cast[HKEY](-1):
-          # Then read the port name from the registry
-          yield getUnicodeValue("", "PortName", regKey)
-
-      inc(index)
-  finally:
-    # Destroy the "device info set" once done with it
-    if not SetupDiDestroyDeviceInfoList(devInfoSet):
-      raiseOsError(osLastError())
---
+++ src/serial/private/utils/windows_registry.nim
@@ -0,0 +1,131 @@
+#
+# mp035: modified version of the registry module from the Nim standard library
+#
+
+#
+#
+#            Nim's Runtime Library
+#        (c) Copyright 2016 Andreas Rumpf
+#
+#    See the file "copying.txt", included in this
+#    distribution, for details about the copyright.
+#
+
+## This module is experimental and its interface may change.
+
+import winlean, os
+
+type
+  HKEY* = uint
+
+const
+  HKEY_LOCAL_MACHINE* = HKEY(0x80000002u)
+  HKEY_CURRENT_USER* = HKEY(2147483649)
+
+  RRF_RT_ANY = 0x0000ffff
+  RRF_RT_REG_DWORD = 0x00000010
+  KEY_WOW64_64KEY = 0x0100
+  KEY_WOW64_32KEY = 0x0200
+  KEY_READ = 0x00020019
+  REG_SZ = 1
+
+proc regOpenKeyEx(hKey: HKEY, lpSubKey: WideCString, ulOptions: int32,
+                  samDesired: int32,
+                  phkResult: var HKEY): int32 {.
+  importc: "RegOpenKeyExW", dynlib: "Advapi32.dll", stdcall.}
+
+proc regCloseKey(hkey: HKEY): int32 {.
+  importc: "RegCloseKey", dynlib: "Advapi32.dll", stdcall.}
+
+proc regGetValue(key: HKEY, lpSubKey, lpValue: WideCString;
+                 dwFlags: int32 = RRF_RT_ANY, pdwType: ptr int32,
+                 pvData: pointer,
+                 pcbData: ptr int32): int32 {.
+  importc: "RegGetValueW", dynlib: "Advapi32.dll", stdcall.}
+
+proc regQueryInfoKey( hKey: HKEY,
+                       lpClass : WideCstring,
+                       lpcchClass : ptr int32,
+                       lpReserved : ptr int32,
+                       lpcSubKeys : ptr int32,
+                       lpcbMaxSubKeyLen : ptr int32,
+                       lpcbMaxClassLen : ptr int32,
+                       lpcValues : ptr int32,
+                       lpcbMaxValueNameLen : ptr int32,
+                       lpcbMaxValueLen : ptr int32,
+                       lpcbSecurityDescriptor : ptr int32,
+                       lpftLastWriteTime : ptr FILETIME): int32 {.
+  importc: "RegQueryInfoKeyW", dynlib: "Advapi32.dll", stdcall.}
+
+proc regEnumValue( hKey: HKEY,
+                    dwIndex : int32,
+                    lpValueName : WideCString,
+                    lpcchValueName : ptr int32,
+                    lpReserved : ptr int32,
+                    lpType : ptr int32,
+                    lpData : pointer,
+                    lpcbData : ptr int32): int32 {.
+  importc: "RegEnumValueW", dynlib: "Advapi32.dll", stdcall.}
+
+template call(f) =
+  let err = f
+  if err != 0:
+    raiseOSError(err.OSErrorCode, astToStr(f))
+
+iterator enumKeyValues*(path: string, handle: HKEY): tuple[key:string, value:string] =
+  var numValues : int32
+  let hh = newWideCString path
+
+  var newHandle: HKEY
+  call regOpenKeyEx(handle, hh, 0, KEY_READ or KEY_WOW64_64KEY, newHandle)
+  call regQueryInfoKey(newHandle, nil, nil, nil, nil, nil, nil, addr numValues, nil, nil, nil, nil)
+  var regNameSize = 16383'i32
+  var regName = newWideCString("", regNameSize)
+  for i in 0 ..< numValues:
+    var regValueSize = 0'i32
+    call regEnumValue(newHandle, int32(i), regName, addr regNameSize, nil, nil, nil, addr regValueSize)
+    var regValue = newWideCString("", regValueSize)
+    regNameSize += 2 # reallocate for null wchar.
+    call regEnumValue(newHandle, int32(i), regName, addr regNameSize, nil, nil, cast[pointer](regValue), addr regValueSize)
+    yield (regName $ regNameSize, regValue $ regValueSize)
+  
+  call regCloseKey(newHandle)
+
+proc getDwordValue*(path, key: string; handle: HKEY): int32 =
+  let hh = newWideCString path
+  let kk = newWideCString key
+  var buffsize = 4'i32
+  var flags: int32 = RRF_RT_REG_DWORD
+  call regGetValue(handle, hh, kk, flags, nil, addr result, addr buffsize)
+
+proc getUnicodeValue*(path, key: string; handle: HKEY): string =
+  let hh = newWideCString path
+  let kk = newWideCString key
+  var bufsize: int32
+  # try a couple of different flag settings:
+  var flags: int32 = RRF_RT_ANY
+  let err = regGetValue(handle, hh, kk, flags, nil, nil, addr bufsize)
+  if err != 0:
+    var newHandle: HKEY
+    call regOpenKeyEx(handle, hh, 0, KEY_READ or KEY_WOW64_64KEY, newHandle)
+    call regGetValue(newHandle, nil, kk, flags, nil, nil, addr bufsize)
+    var res = newWideCString("", bufsize)
+    call regGetValue(newHandle, nil, kk, flags, nil, cast[pointer](res),
+                   addr bufsize)
+    result = res $ bufsize
+    call regCloseKey(newHandle)
+  else:
+    var res = newWideCString("", bufsize)
+    call regGetValue(handle, hh, kk, flags, nil, cast[pointer](res),
+                   addr bufsize)
+    result = res $ bufsize
+
+proc regSetValue(key: HKEY, lpSubKey, lpValueName: WideCString,
+                 dwType: int32; lpData: WideCString; cbData: int32): int32 {.
+  importc: "RegSetKeyValueW", dynlib: "Advapi32.dll", stdcall.}
+
+proc setUnicodeValue*(path, key, val: string; handle: HKEY) =
+  let hh = newWideCString path
+  let kk = newWideCString key
+  let vv = newWideCString val
+  call regSetValue(handle, hh, kk, REG_SZ, vv, (vv.len.int32+1)*2)
